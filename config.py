4a8c04d993b6b3ccfaabd7687614b926f09d2483
KhetanKvr <91938784+KhetanKvr@users.noreply.github.com>
2023-01-29 15:57:46 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "cloudproject.ccyuwnl3zoe8.us-east-1.rds.amazonaws.com"
 customuser = "cloudgroup38"
 custompass = "cloudgroup38"
-customdb = "employee"
+customdb = "employeee"
 custombucket = "cloudprojectadd"
 customregion = "us-east-1"
